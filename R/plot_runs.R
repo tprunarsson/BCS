deb9e842332c4f59339d0de014398d4274581db8
Tómas Philip Rúnarsson <tpr@Sleipnir.lan>
2020-03-31 12:47:06 +0000

    update

--- R/plot_runs.R
+++ R/plot_runs.R
@@ -1,13 +1,13 @@
 library(ggplot2)
 
 path_data <-'covid_run_0_2020-03-30.csv'
-median_run <- read.csv2(file = path_data, sep=",")
-median_run$day = as.factor(median_run$day)
-ggplot(median_run, aes(x = day, y = home)) + geom_boxplot()
-ggplot(median_run, aes(x = day, y = inpatient_ward)) + geom_boxplot()
-ggplot(median_run, aes(x = day, y = intensive_care_unit)) + geom_boxplot()
-ggplot(median_run, aes(x = day, y = death)) + geom_boxplot()
-ggplot(median_run, aes(x = day, y = recovered)) + geom_boxplot()
+upper_run <- read.csv2(file = path_data, sep=",")
+upper_run$day = as.factor(median_run$day)
+ggplot(upper_run, aes(x = day, y = home)) + geom_boxplot()
+ggplot(upper_run, aes(x = day, y = inpatient_ward)) + geom_boxplot()
+ggplot(upper_run, aes(x = day, y = intensive_care_unit)) + geom_boxplot()
+ggplot(upper_run, aes(x = day, y = death)) + geom_boxplot()
+ggplot(upper_run, aes(x = day, y = recovered)) + geom_boxplot()
 
 path_data <-'covid_run_1_2020-03-30.csv'
 median_run <- read.csv2(file = path_data, sep=",")
