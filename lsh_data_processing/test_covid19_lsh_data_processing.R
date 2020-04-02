f297dec88a1012ee65258f79b1e1eaeb2da4345e
Rögnvaldur Saemundsson <rsaemundsson@gmail.com>
2020-04-02 21:41:36 +0000

    Added tests for data file

--- lsh_data_processing/test_covid19_lsh_data_processing.R
+++ lsh_data_processing/test_covid19_lsh_data_processing.R
@@ -4,7 +4,49 @@ cmp_vectors <- function(x1,x2){
 
 
 test_lsh_data_file <- function(){
+    #Check if new sheets or changes to sheet names
+    distinct_s_data <- distinct(tibble(excel_sheets(file_path_data)) %>% rename(sheet_name_raw=`excel_sheets(file_path_data)`))
+    distinct_s_coding <- distinct(sheet_names,sheet_name_raw)
     
+    if ((distinct_s_data %>% summarize(n()) %>% unlist()) > 
+        (distinct_s_coding %>% summarize(n()) %>% unlist())) {
+        warning('BCS:New sheet(s) have been added to the LSH data file')
+        do.call(cat,c(setdiff(distinct_s_data,distinct_s_coding),sep='\n'))
+    }
+    
+    if (identical(sheets_in_datafile,sheet_names)) {
+        warning('BCS: Sheet names have changed')
+    }
+    
+    #Check if new unit categories
+    distinct_uc_data <- distinct(rename(hospital_visits_raw, unit_category_raw=`Deild Heiti`),unit_category_raw) %>% filter(!is.na(unit_category_raw))
+    distinct_uc_coding <- distinct(unit_categories,unit_category_raw)
+    
+    if ((distinct_uc_data %>% summarize(n()) %>% unlist()) > 
+        (distinct_uc_coding %>% summarize(n()) %>% unlist())) {
+        warning('BCS:New unit categories have been added to the LSH data file')
+        do.call(cat,c(setdiff(distinct_uc_data,distinct_uc_coding),sep='\n'))
+    }
+    
+    #Check if new text out categories
+    distinct_toc_data <- distinct(rename(hospital_visits_raw, text_out_category_raw=`Heiti afdrifa`),text_out_category_raw) %>% filter(!is.na(text_out_category_raw))
+    distinct_toc_coding <- distinct(text_out_categories,text_out_category_raw)
+    
+    if ((distinct_toc_data %>% summarize(n()) %>% unlist()) > 
+        (distinct_toc_coding %>% summarize(n()) %>% unlist())) {
+        warning('BCS:New text out categories have been added to the LSH data file')
+        do.call(cat,c(setdiff(distinct_toc_data,distinct_toc_coding),sep='\n'))
+    }
+    
+    #Check if new Covid groups
+    distinct_cg_data <- distinct(rename(individs_raw, covid_group_raw=`Heiti sjúklingahóps`),covid_group_raw) %>% filter(!is.na(covid_group_raw))
+    distinct_cg_coding <- distinct(covid_groups,covid_group_raw)
+    
+    if ((distinct_cg_data %>% summarize(n()) %>% unlist()) > 
+        (distinct_cg_coding %>% summarize(n()) %>% unlist())) {
+        warning('BCS:New COVID19 groups have been added to the LSH data file')
+        do.call(cat,c(setdiff(distinct_cg_data,distinct_cg_coding),sep='\n'))
+    }
 }
 
 test_cleaning <- function(){
