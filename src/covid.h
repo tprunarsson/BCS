ce0201d0fe5c85778a76ac7c2ccf96b37e7488d8
Tómas Philip Rúnarsson <tpr@Sleipnir.lan>
2020-04-04 19:53:19 +0000

    heusistic update and age bug fix

--- src/covid.h
+++ src/covid.h
@@ -2,6 +2,7 @@
 
 #define MAXINFECTED 65536 /* estimated MAX value infected! */
 #define MAX_INFECTED_PER_DAY 8192 /* estimated MAX value infected! */
+#define MAX_REPEAT 1000 /* number of times we replicated the simulation */
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
@@ -9,6 +10,7 @@
 #define MAX_AGE_GROUPS       2
 #define MAX_LOS_DAYS        60
 #define MAX_SIM_TIME        28
+#define MAX_HOME4LSH        24
 
 #define EVENT_ARRIVAL        1
 #define EVENT_DEPARTURE      2
@@ -17,12 +19,14 @@
 #define STREAM_AGE          2  /* Random-number stream for service times. */
 
 #define ATTR_AGEGROUP       2 /* index in transfer for this attribute */
-#define ATTR_DAYS           3 
+#define ATTR_DAYSDIAG       3 
 #define ATTR_GENDER         4
 #define ATTR_DAYSINLOC      5
 #define ATTR_LOCATION       6
 #define ATTR_DEPARTDAY      7
 #define ATTR_PERSON         8
+#define ATTR_NEXTLOCATION   9
+#define ATTR_LASTWORSTLOC  10
 
 // home,emergency_room,outpatient_clinic,inpatient_ward,intensive_care_unit,death,recovered
 
