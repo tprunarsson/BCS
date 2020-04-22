fff5afe5a071acd7d980b3383aad22d309f24ca5
Rögnvaldur Saemundsson <rsaemundsson@gmail.com>
2020-04-22 12:31:02 +0000

    changes dates and flags

--- lsh_data_processing/covid19_lsh_data_processing.R
+++ lsh_data_processing/covid19_lsh_data_processing.R
@@ -9,11 +9,11 @@ source('test_covid19_lsh_data_processing.R')
 source('create_input_for_simulation.R')
 source('help_functions.R')
 
-current_date_tmp <- as.Date('2020-04-21','%Y-%m-%d')
+current_date_tmp <- as.Date('2020-04-15','%Y-%m-%d')
 prediction_date_tmp <- as.Date('2020-04-14','%Y-%m-%d')
 path_to_lsh_data_tmp <- '~/projects/covid/BCS/lsh_data/'
 #path_to_lsh_data_tmp <- '../../'
-write_tables_tmp <- FALSE
+write_tables_tmp <- TRUE
 run_id_tmp <- 1
 
 max_num_days_inpatient_ward <- 28
