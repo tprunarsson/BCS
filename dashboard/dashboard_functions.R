2a3f79cd8544792d197ad5c34887f667ce5641e5
khelgason <krihel@gmail.com>
2020-04-24 11:49:33 +0000

    New dashboard version. Adds styles, functions and rds use

--- dashboard/dashboard_functions.R
+++ dashboard/dashboard_functions.R
@@ -10,7 +10,7 @@ palette1 <- c("#E69F00", "#56B4E9", "#CC79A7", "#009E73",
 #'
 #' @return
 #' @export
-load_git_data <- function(type = c('length_of_stay_with_imputed', 'length_of_stay_empirical', 'transition_matrix', 'covid_simulation', 'prop_outpatient_clinic'),
+load_git_data <- function(type = c('length_of_stay_with_imputed', 'length_of_stay_with_imputed_age_simple', 'length_of_stay_empirical', 'transition_matrix', 'covid_simulation', 'prop_outpatient_clinic'),
                           data_folder = 'dashboard/input', 
                           start_date = '2020-04-02') {
     # Create URLs to csv files
@@ -20,14 +20,17 @@ load_git_data <- function(type = c('length_of_stay_with_imputed', 'length_of_sta
     #Get csv data to a list and row-bind all non-null results
     data_list <- list()
     for (file in files) {
-        data_list[[file]] <- try(
-            read.csv(file.path(root_path, data_folder, file)), silent = TRUE
-        )
+        github_url <- file.path(root_path, data_folder, file)
+        if (RCurl::url.exists(github_url)) {
+            data_list[[file]] <- try(read.csv(github_url), silent = TRUE)
+        } else {
+            next
+        }
         # Add state column for transition data
         if (grepl('transition', type)) {
             data_list[[file]]$state <- names(data_list[[file]])
         }
-        # Add date column if file existed. Remove crom list if file didn't exist
+        # Add date column if file existed. Remove from list if file didn't exist
         if (typeof(data_list[[file]]) == 'list') {
             data_list[[file]]$date <- as.Date(str_extract(file, '\\d{4}-\\d{2}-\\d{2}'))
         } else {
@@ -45,16 +48,19 @@ load_git_data <- function(type = c('length_of_stay_with_imputed', 'length_of_sta
 #' @return
 #' @export
 recode_states <- function(df_with_state) {
-    stopifnot('state' %in% names(df_with_state))
-    df_with_state %>% 
-        mutate(state = state %>% recode(home='Heimaeinangrun',
-                                        inpatient_ward='Legudeild',
-                                        intensive_care_unit='Gjörgæsla'))
+    # stopifnot(nrow(df_with_state)>0)
+    # stopifnot('state' %in% names(df_with_state))
+    try(
+        df_with_state %>% 
+            mutate(state = state %>% recode(home='Heimaeinangrun',
+                                            outpatient='Göngudeild',
+                                            inpatient_ward='Legudeild',
+                                            intensive_care_unit='Gjörgæsla')),
+    silent = T)
 }
 
 
-
-get_and_save_data <- function() {
+initial_get_and_save_data <- function() {
     bcs_data <- list()
     bcs_data$data_length_of_stay <- load_git_data(
         'length_of_stay_empirical', 
@@ -73,31 +79,51 @@ get_and_save_data <- function() {
 }
 
 
-get_and_save_if_new_data <- function(bcs_data = NULL) {
-    max_date_los <- coalesce(max(bcs_data$data_length_of_stay$date,'2020-04-08'))
-    max_date_sim <- coalesce(max(bcs_data$data_all_simulation$date,'2020-04-09'))
+get_and_save_new_data <- function(bcs_data) {
+    max_date_los <- max(bcs_data$data_length_of_stay$date)
+    max_date_sim <- max(bcs_data$data_all_simulation$date)
+    showNotification('Sæki ný gögn...', duration = 4, id = 'msg_saeki', type = 'message')
+    data_simulation_new <- data.frame()
+    data_los_new <- data.frame()
     if (max_date_sim < Sys.Date()) {
         data_simulation_new <- load_git_data(
             'covid_simulation',
             data_folder = 'simulation_history',
             start_date = max_date_sim + 1)
+    }
+    if (nrow(data_simulation_new) > 0) {
         bcs_data$data_all_simulation <- bind_rows(bcs_data$data_all_simulation,
                                                   data_simulation_new)
     }
+
     if (max_date_los < Sys.Date()) {
         data_los_new <- load_git_data(
-            'length_of_stay_empirical', 
-            start_date = max_date_los + 1
-        ) %>% 
-            recode_states
-        bcs_data$data_length_of_stay <- bind_rows(bcs_data$data_length_of_stay,
-                                                  data_los_new)
-        
+            'length_of_stay_empirical_age_simple', 
+            start_date = max_date_los + 1)
+        if (nrow(data_los_new) > 0) {
+            data_los_new <- data_los_new %>% 
+                recode_states()%>% 
+                mutate(age_group_simple = gsub('age_', '', splitting_variable)) %>% 
+                select(-splitting_variable)
+            bcs_data$data_length_of_stay <- bind_rows(bcs_data$data_length_of_stay,
+                                                      data_los_new)
+        }
     }
-    if (any(exists('data_simulation_new'), exists('data_los_new'))) {
+    if (any(nrow(data_simulation_new) > 0, 
+            nrow(data_los_new) > 0)) {
         saveRDS(bcs_data, 'input/bcs_data.rds')
-        message('New data has been saved')
+        msg <- 'Ný gögn hafa verið vistuð'
     } else {
-        message('Data is already up to date')
+        msg <- 'Engin nýrri gögn fundust'
     }
+    message(msg)
+    return(msg)
+}
+
+
+join_prop_outpatient_to_sim <- function(window_size = 7) {
+    df_with_props <- bcs_data$data_all_simulation %>% 
+        mutate(date_prop = date - window_size) %>% 
+        dplyr::full_join(bcs_data$prop_visits, by = c('date_prop' = 'date')) %>% 
+        mutate(outpatient = home * prop_visits_last_week)
 }
