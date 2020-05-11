1e1bb6ef11620f6293a45157753b6a9d46e0861d
Sölvi Rögnvaldsson <sor16@hi.is>
2020-05-11 20:16:00 +0000

    created execute_run_dummy and shiny_validation to prepare shiny app for release

--- experiments/execute_run_dummy.sh
+++ experiments/execute_run_dummy.sh
@@ -23,6 +23,7 @@ date_start_default="2020-03-02"
 date_end_default="2020-04-20"
 use_scenario_default=""
 use_historical_default="0"
+date_data_default="2020-04-20"
 
 date_start=${date_start:-"$date_start_default"}
 date_end=${date_end:-"$date_end_default"}
@@ -30,13 +31,5 @@ use_scenario=${use_scenario:-"$use_scenario_default"}
 use_historical=${use_historical:-"$use_historical_default"}
 num_sim_days=$(Rscript -e 'args <- commandArgs(trailingOnly = TRUE);print(as.numeric(as.Date(args[2])-as.Date(args[1])))' "$date_start" "$date_end" | awk '{print $2}')
 
-cd lsh_data_processing
-Rscript covid19_lsh_data_processing.R -c "$date_data" -r "$run_id" -s "$date_start"
-
-cd ../src
-make covid
-while read experiment_id model splitting_variable_name splitting_variable_values heuristic_string
- do ./covid -s "$date_start" -i "$experiment_id" -h "$heuristic_string" -v "$splitting_variable_values" -d "$date_data" -n "$num_sim_days" -p .. -r "$use_historical" "$use_scenario"
-done <  ../input/"$date_data"_"$run_id"_run_info.csv
-cd ../lsh_data_processing
-Rscript ../R/run_evaluation.R -r "$run_id" -d "$date_data" -s "$date_start"
+cd experiments/
+Rscript shiny_validation.R -r "$run_id" -s "$date_start" "$use_scenario"
---
+++ experiments/shiny_validation.R
@@ -0,0 +1,82 @@
+#!/usr/bin/env Rscript
+invisible(Sys.setlocale("LC_ALL","IS_is"))
+suppressPackageStartupMessages({
+    require(optparse)
+    require(readxl)
+    require(dplyr)
+    require(tidyr)
+    require(readr)
+})
+
+option_list <-  list(
+    make_option(c("-s", "--date_start"), type="character", default=NULL, 
+                help="start date of simulation", metavar="character"),
+    make_option(c("-r", "--run_id"), type="integer", default=NULL, 
+                help="run_id to identify run of a set of experiments", metavar="integer"),
+    make_option(c("-a", "--use_scenario"),action='store_true', default=FALSE, 
+                help="scenario used or not")
+    
+)
+
+opt_parser <-  OptionParser(option_list=option_list);
+opt <-  parse_args(opt_parser);
+
+if(is.null(opt[['date_start']])){
+    stop(paste0('Error: You must provide a start date. '))
+}else{
+    date_start <- opt[['date_start']]
+}
+if(is.null(opt[['run_id']])){
+    stop(paste0('Error: You must provide a run_id'))
+}else{
+    run_id <- 1
+}
+if(is.null(opt[['use_scenario']])){
+    stop(paste0('use_scenario flag must be active'))
+}
+scenario_first_state_filename <- paste0(date_start,'_',run_id,'_scenario_first_state.csv')
+scenario_infected_filename <- paste0(date_start,'_',run_id,'_scenario_infected.csv')
+path_input <- '../input/'
+if(!(scenario_first_state_filename %in% list.files(path_input))){
+    stop('Error: scenario first state file does not exist')
+}else if(!(scenario_infected_filename %in% list.files(path_input))){
+    stop('Error: scenario infected file does not exist')
+}else{
+    scenario_first_state <- read_csv(paste0(path_input,scenario_first_state_filename),col_types=cols())
+    scenario_infected <- read_csv(paste0(path_input,scenario_infected_filename),col_types=cols())
+}
+
+
+###### scenario_infected tests ##################
+if(class(scenario_infected$date_diagnosis)!='Date'){
+    stop('Error: date_diagnosis column in scenario_infected must be of class Date')
+}else if(any(scenario_infected$count%%1!=0)){
+    stop('Error: count column in scenario_infected must be an integer')
+}
+
+if(min(scenario_infected$date_diagnosis)!=date_start){
+    stop('Error: start date column in scenario_infected file must match date_start input argument') 
+}
+
+###### scenario_first_state tests ##################
+if(class(scenario_first_state$initial_state)!='character'){
+    stop('Error: initial_state column in scenario_first_state must be of class character')
+}else if(class(scenario_first_state$splitting_variable)!='character'){
+    stop('Error: splitting_variable column in scenario_first_state must be of class character')
+}else if(any(scenario_first_state$count%%1!=0)){
+    stop('Error: count column in scenario_infected must be an integer')
+}
+
+if(any(scenario_first_state$initial_state!=c('home','home','inpatient_ward','inpatient_ward','intensive_care_unit','intensive_care_unit'))){
+    stop('Error: initial_state column in scenario_first_state file is in wrong format') 
+}else if(any(scenario_first_state$splitting_variable!=c('age_0-50','age_51+','age_0-50','age_51+','age_0-50','age_51+'))){
+    stop('Error: splitting_variable column in scenario_first_state file is in wrong format')
+}
+
+print('tests_passed')
+
+
+
+
+
+
